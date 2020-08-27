459dbda0c8140eaf97a0c8a095c6c85b535f348d
Abel Esquivel <abelesquivel94@gmail.com>
2020-08-27 14:16:41 +0100

    configuraciónDelProyecto

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import moduleName from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld'
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'))
